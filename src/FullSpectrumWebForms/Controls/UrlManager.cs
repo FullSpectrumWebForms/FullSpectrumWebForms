55ba597125865f9a3a35843fe0611cde78cf2caa
pascal.arnold <pascal.arnold@genikinc.com>
2018-08-13 11:37:25 -0400

    Working on UrlManager.cs

---
+++ src/FullSpectrumWebForms/Controls/UrlManager.cs
@@ -0,0 +1,16 @@
+﻿using FSW.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSW.Controls
+{
+    public class UrlManager : ControlBase
+    {
+        public override void InitializeProperties()
+        {
+
+        }
+    }
+}
