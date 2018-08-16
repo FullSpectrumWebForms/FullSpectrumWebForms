49a8e38575a788076f26a2b231d6e61e1660d49b
pascx64 <pascx64@gmail.com>
2018-08-16 19:52:08 -0400

    Added empty FullSpectrumWebForms_UnitTests library

---
+++ src/FullSpectrumWebForms_UnitTests/Controls/UTManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FSW_UnitTests.Controls
+{
+    public class UTManager : FSW.Core.ControlBase
+    {
+        public override void InitializeProperties()
+        {
+
+        }
+
+
+    }
+}
---
+++ src/FullSpectrumWebForms_UnitTests/wwwroot/js/unitTests/controls/UTManager.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="../../../../../fullSpectrumWebForms/wwwroot/js/core/controlBase.ts" />
+namespace controls {
+
+    export class UTManager extends core.controlBase {
+        
+        initialize(type: string, index: number, id: string, properties: { property: string, value: any }[]) {
+            super.initialize(type, index, id, properties);
+            
+        }
+    }
+}
+core.controlTypes['UTManager'] = () => new controls.UTManager();
