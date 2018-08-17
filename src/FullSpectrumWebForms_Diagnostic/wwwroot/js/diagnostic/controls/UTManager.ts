18ed1a366daf55d4f1ee7936d4f9b99f15924f69
pascal.arnold <pascal.arnold@genikinc.com>
2018-08-17 16:06:38 -0400

    Added back diagnostic/controls/UTManager.ts and FullSpectrumWebForms_Diagnostic/FullSpectrumWebForms_Diagnostic.csproj

---
+++ src/FullSpectrumWebForms_Diagnostic/wwwroot/js/diagnostic/controls/UTManager.ts
@@ -0,0 +1,52 @@
+﻿/// <reference path="../../../../../fullSpectrumWebForms/wwwroot/js/core/controlBase.ts" />
+/// <reference path="../../../../../fullSpectrumWebForms/wwwroot/js/controls/html/htmlControlBase.ts" />
+/// <reference path="../../../../node_modules/@types/jquery/index.d.ts" />
+
+namespace controls {
+
+    export class UTManager extends core.controlBase {
+        
+        initialize(type: string, index: number, id: string, properties: { property: string, value: any }[]) {
+            super.initialize(type, index, id, properties);
+        }
+
+        checkIfElementExists(id: string) {
+            let elem = $('#' + id);
+            return elem.length != 0;
+        }
+
+        getElementTextFromId(id: string) {
+            let elem = $('#' + id);
+            if (elem.length == 0)
+                return null;
+            return elem.text();
+        }
+
+        getFSWControlText(id: string) {
+            let control = core.manager.getControl(id) as html.htmlControlBase;
+            if (control && control.element) {
+                return control.element.val();
+            }
+            else
+                return null;
+        }
+
+
+        getElementValFromId(id: string) {
+            let elem = $('#' + id);
+            if (elem.length == 0)
+                return null;
+            return elem.text();
+        }
+
+        getFSWControlVal(id: string) {
+            let control = core.manager.getControl(id) as html.htmlControlBase;
+            if (control && control.element) {
+                return control.element.val();
+            }
+            else
+                return null;
+        }
+    }
+}
+core.controlTypes['UTManager'] = () => new controls.UTManager();
