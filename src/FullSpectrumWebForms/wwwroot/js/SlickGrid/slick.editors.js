ad3f835051e1f9e629518d389e10dae7bc230178
pascx64 <pascx64@gmail.com>
2018-09-01 11:20:39 -0400

    Updated slickgrid and fixed select2 scroll bug

--- src/FullSpectrumWebForms/wwwroot/js/SlickGrid/slick.editors.js
+++ src/FullSpectrumWebForms/wwwroot/js/SlickGrid/slick.editors.js
@@ -11,7 +11,7 @@
       "Editors": {
         "Text": TextEditor,
         "Integer": IntegerEditor,
-		"Float": FloatEditor,
+        "Float": FloatEditor,
         "Date": DateEditor,
         "YesNoSelect": YesNoSelectEditor,
         "Checkbox": CheckboxEditor,
@@ -27,13 +27,10 @@
     var scope = this;
 
     this.init = function () {
+      var navOnLR = args.grid.getOptions().editorCellNavOnLRKeys;
       $input = $("<INPUT type=text class='editor-text' />")
           .appendTo(args.container)
-          .on("keydown.nav", function (e) {
-            if (e.keyCode === $.ui.keyCode.LEFT || e.keyCode === $.ui.keyCode.RIGHT) {
-              e.stopImmediatePropagation();
-            }
-          })
+          .on("keydown.nav", navOnLR ? handleKeydownLRNav : handleKeydownLRNoNav)
           .focus()
           .select();
     };
@@ -96,16 +93,12 @@
     var scope = this;
 
     this.init = function () {
-      $input = $("<INPUT type=text class='editor-text' />");
-
-      $input.on("keydown.nav", function (e) {
-        if (e.keyCode === $.ui.keyCode.LEFT || e.keyCode === $.ui.keyCode.RIGHT) {
-          e.stopImmediatePropagation();
-        }
-      });
-
-      $input.appendTo(args.container);
-      $input.focus().select();
+      var navOnLR = args.grid.getOptions().editorCellNavOnLRKeys;
+      $input = $("<INPUT type=text class='editor-text' />")
+      .appendTo(args.container)
+      .on("keydown.nav", navOnLR ? handleKeydownLRNav : handleKeydownLRNoNav)      
+      .focus()
+      .select();
     };
 
     this.destroy = function () {
@@ -165,16 +158,12 @@
     var scope = this;
 
     this.init = function () {
-      $input = $("<INPUT type=text class='editor-text' />");
-
-      $input.on("keydown.nav", function (e) {
-        if (e.keyCode === $.ui.keyCode.LEFT || e.keyCode === $.ui.keyCode.RIGHT) {
-          e.stopImmediatePropagation();
-        }
-      });
-
-      $input.appendTo(args.container);
-      $input.focus().select();
+      var navOnLR = args.grid.getOptions().editorCellNavOnLRKeys;
+      $input = $("<INPUT type=text class='editor-text' />")
+      .appendTo(args.container)
+      .on("keydown.nav", navOnLR ? handleKeydownLRNav : handleKeydownLRNoNav)      
+      .focus()
+      .select();
     };
 
     this.destroy = function () {
@@ -185,24 +174,24 @@
       $input.focus();
     };
 
-	function getDecimalPlaces() {
-		// returns the number of fixed decimal places or null
-		var rtn = args.column.editorFixedDecimalPlaces;
-		if (typeof rtn == 'undefined') {
-			rtn = FloatEditor.DefaultDecimalPlaces;
-		}
-		return (!rtn && rtn!==0 ? null : rtn);
-	}
+    function getDecimalPlaces() {
+        // returns the number of fixed decimal places or null
+        var rtn = args.column.editorFixedDecimalPlaces;
+        if (typeof rtn == 'undefined') {
+            rtn = FloatEditor.DefaultDecimalPlaces;
+        }
+        return (!rtn && rtn!==0 ? null : rtn);
+    }
 
     this.loadValue = function (item) {
       defaultValue = item[args.column.field];
 
-	  var decPlaces = getDecimalPlaces();
-	  if (decPlaces !== null
-	  && (defaultValue || defaultValue===0)
-	  && defaultValue.toFixed) {
-		defaultValue = defaultValue.toFixed(decPlaces);
-	  }
+      var decPlaces = getDecimalPlaces();
+      if (decPlaces !== null
+      && (defaultValue || defaultValue===0)
+      && defaultValue.toFixed) {
+        defaultValue = defaultValue.toFixed(decPlaces);
+      }
 
       $input.val(defaultValue);
       $input[0].defaultValue = defaultValue;
@@ -210,14 +199,19 @@
     };
 
     this.serializeValue = function () {
-	  var rtn = parseFloat($input.val()) || 0;
+      var rtn = parseFloat($input.val());
+      if (FloatEditor.AllowEmptyValue) {
+        if (!rtn && rtn !==0) { rtn = ''; }
+      } else {
+        rtn = rtn || 0;
+      }
 
-	  var decPlaces = getDecimalPlaces();
-	  if (decPlaces !== null
-	  && (rtn || rtn===0)
-	  && rtn.toFixed) {
-		rtn = parseFloat(rtn.toFixed(decPlaces));
-	  }
+      var decPlaces = getDecimalPlaces();
+      if (decPlaces !== null
+      && (rtn || rtn===0)
+      && rtn.toFixed) {
+        rtn = parseFloat(rtn.toFixed(decPlaces));
+      }
 
       return rtn;
     };
@@ -255,6 +249,7 @@
   }
 
   FloatEditor.DefaultDecimalPlaces = null;
+  FloatEditor.AllowEmptyValue = false;
 
   function DateEditor(args) {
     var $input;
@@ -269,8 +264,7 @@
       $input.datepicker({
         showOn: "button",
         buttonImageOnly: true,
-        buttonImage: "../images/calendar.gif",
-        beforeShow: function () {
+         beforeShow: function () {
           calendarOpen = true
         },
         onClose: function () {
@@ -422,6 +416,10 @@
       }
     };
 
+    this.preClick = function () {
+        $select.prop('checked', !$select.prop('checked'));
+    };
+
     this.serializeValue = function () {
       return $select.prop('checked');
     };
@@ -531,11 +529,12 @@
 
     this.init = function () {
       var $container = $("body");
+      var navOnLR = args.grid.getOptions().editorCellNavOnLRKeys;
 
       $wrapper = $("<DIV style='z-index:10000;position:absolute;background:white;padding:5px;border:3px solid gray; -moz-border-radius:10px; border-radius:10px;'/>")
           .appendTo($container);
 
-      $input = $("<TEXTAREA hidefocus rows=5 style='backround:white;width:250px;height:80px;border:0;outline:0'>")
+      $input = $("<TEXTAREA hidefocus rows=5 style='background:white;width:250px;height:80px;border:0;outline:0'>")
           .appendTo($wrapper);
 
       $("<DIV style='text-align:right'><BUTTON>Save</BUTTON><BUTTON>Cancel</BUTTON></DIV>")
@@ -543,7 +542,7 @@
 
       $wrapper.find("button:first").on("click", this.save);
       $wrapper.find("button:last").on("click", this.cancel);
-      $input.on("keydown", this.handleKeyDown);
+      $input.on("keydown", this.handleKeyDown); 
 
       scope.position(args.position);
       $input.focus().select();
@@ -561,6 +560,17 @@
       } else if (e.which == $.ui.keyCode.TAB) {
         e.preventDefault();
         args.grid.navigateNext();
+      } else if (e.which == $.ui.keyCode.LEFT || e.which == $.ui.keyCode.RIGHT) {
+        if (args.grid.getOptions().editorCellNavOnLRKeys) {
+          var cursorPosition = this.selectionStart;
+          var textLength = this.value.length;
+          if (e.keyCode === $.ui.keyCode.LEFT && cursorPosition === 0) {
+            args.grid.navigatePrev();
+          }
+          if (e.keyCode === $.ui.keyCode.RIGHT && cursorPosition >= textLength-1) {
+            args.grid.navigateNext();
+          }
+        }
       }
     };
 
@@ -628,4 +638,25 @@
 
     this.init();
   }
+  
+  /*
+   * Depending on the value of Grid option 'editorCellNavOnLRKeys', us 
+   * Navigate to the cell on the left if the cursor is at the beginning of the input string
+   * and to the right cell if it's at the end. Otherwise, move the cursor within the text
+   */
+  function handleKeydownLRNav(e) {
+    var cursorPosition = this.selectionStart;
+    var textLength = this.value.length;
+    if ((e.keyCode === $.ui.keyCode.LEFT && cursorPosition > 0) ||
+         e.keyCode === $.ui.keyCode.RIGHT && cursorPosition < textLength-1) {
+      e.stopImmediatePropagation();
+    }
+  }
+
+  function handleKeydownLRNoNav(e) {
+    if (e.keyCode === $.ui.keyCode.LEFT || e.keyCode === $.ui.keyCode.RIGHT) {	
+      e.stopImmediatePropagation();	
+    }	
+  }
+  
 })(jQuery);
