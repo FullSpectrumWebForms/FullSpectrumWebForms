835b5164e0516ec4f5bf6acc119430578bcac447
Olivier Bastien <willow__18@hotmail.com>
2018-11-15 20:26:41 -0500

    Correction img dans img viewer

--- src/FullSpectrumWebForms/wwwroot/js/controls/html/viewerjs.ts
+++ src/FullSpectrumWebForms/wwwroot/js/controls/html/viewerjs.ts
@@ -40,13 +40,14 @@ namespace controls.html {
             var items = this.Items;
             for (let i = 0; i < items.length; ++i) {
 
-                let li = $('<li></li>');
+                let img = $('<img></img>');
 
                 if (items[i].ThumbnailSrc && items[i].ThumbnailSrc != '')
-                    li.attr('src', items[i].ThumbnailSrc);
+                    img.attr('src', items[i].ThumbnailSrc);
 
-                li.attr('data-original', items[i].OriginalSrc);
-                li.appendTo(this.ulElement);
+                img.attr('data-original', items[i].OriginalSrc);
+                img.appendTo($('<li></li>').appendTo(this.ulElement));
+
 
             }
             this.viewer.update();
