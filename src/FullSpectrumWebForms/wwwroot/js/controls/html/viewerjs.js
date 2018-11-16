5b186579a4c31cae7bf2f8cf798c112589b60c02
Olivier Bastien <willow__18@hotmail.com>
2018-11-15 20:09:51 -0500

    changement pour viewerjs

---
+++ src/FullSpectrumWebForms/wwwroot/js/controls/html/viewerjs.js
@@ -0,0 +1,39 @@
+var controls;
+(function (controls) {
+    var html;
+    (function (html) {
+        class viewerjs extends html.htmlControlBase {
+            get Items() {
+                return this.getPropertyValue("Items");
+            }
+            set Items(value) {
+                this.setPropertyValue("Items", value);
+            }
+            initialize(type, index, id, properties) {
+                super.initialize(type, index, id, properties);
+                this.getProperty("Items").onChangedFromServer.register(this.onItemsChangedFromServer.bind(this));
+                this.ulElement = $('<ul></ul>').appendTo(this.element);
+                this.viewer = new Viewer(this.ulElement[0]);
+            }
+            initializeHtmlElement() {
+                this.element = $('<div></div>');
+                this.appendElementToParent();
+            }
+            onItemsChangedFromServer() {
+                this.ulElement.empty();
+                var items = this.Items;
+                for (let i = 0; i < items.length; ++i) {
+                    let li = $('<li></li>');
+                    if (items[i].ThumbnailSrc && items[i].ThumbnailSrc != '')
+                        li.attr('src', items[i].ThumbnailSrc);
+                    li.attr('data-original', items[i].OriginalSrc);
+                    li.appendTo(this.ulElement);
+                }
+                this.viewer.update();
+            }
+        }
+        html.viewerjs = viewerjs;
+    })(html = controls.html || (controls.html = {}));
+})(controls || (controls = {}));
+core.controlTypes['Viewerjs'] = () => new controls.html.viewerjs();
+//# sourceMappingURL=viewerjs.js.map
