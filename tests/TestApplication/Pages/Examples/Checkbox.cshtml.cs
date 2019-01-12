123130900f71222d5d8f3b8443f98dddbc017ad5
Pascal Arnold <pascal.arnold@genikinc.com>
2019-01-12 10:47:04 -0500

    Added Checkbox example

---
+++ tests/TestApplication/Pages/Examples/Checkbox.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestApplication.Pages.Examples
+{
+    public class CheckboxPage : FSW.Core.FSWPage
+    {
+        public FSW.Controls.Html.Checkbox CK_Test = new FSW.Controls.Html.Checkbox();
+        public override void OnPageLoad()
+        {
+            base.OnPageLoad();
+
+            CK_Test.OnCheckedChanged += CK_Test_OnStateChanged;
+        }
+
+        private void CK_Test_OnStateChanged(FSW.Controls.Html.Checkbox sender)
+        {
+            MessageBox.Success("State", CK_Test.Checked ? "Checked!" : "Unchecked!");
+        }
+    }
+}
