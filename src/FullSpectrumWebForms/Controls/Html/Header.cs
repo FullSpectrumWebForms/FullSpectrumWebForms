f2b23f93d57f6f0d291b9d5ab43973729dd05ef4
pascal.arnold <pascal.arnold@genikinc.com>
2019-02-11 08:58:18 -0500

    Added header

---
+++ src/FullSpectrumWebForms/Controls/Html/Header.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSW.Controls.Html
+{
+    public class Header : HtmlControlBase
+    {
+        public int Size
+        {
+            get => int.Parse(HtmlDefaultTag.Remove(0, 1));
+            set => HtmlDefaultTag = $"H{value}";
+        }
+
+        public string Text
+        {
+            get => InnerText;
+            set => InnerText = value;
+        }
+
+        public override string ControlType => nameof(HtmlControlBase);
+
+        public override void InitializeProperties()
+        {
+            base.InitializeProperties();
+
+            Size = "1";
+            Text = "";
+        }
+    }
+}
